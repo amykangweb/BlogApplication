af7c85fc4c35ebd89eaaa18b1a4cad12081dfb5b
amykangweb <selene6023@gmail.com>
2016-06-03 23:52:05 -0700

    Create asp.net app.

---
+++ BlogApplicationUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlogApplicationUI.MvcApplication" Language="C#" %>
---
+++ BlogApplicationUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogApplicationUI.Startup))]
+namespace BlogApplicationUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
