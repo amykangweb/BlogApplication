c0d270dfd2b9a267abb3c6f0fc1674db058e0e3e
amykangweb <selene6023@gmail.com>
2016-06-28 06:32:13 -0700

    Add blog controller and index view.

---
+++ BlogApplicationUI/Controllers/BlogController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BlogApplicationUI.Models;
+
+
+namespace BlogApplicationUI.Controllers
+{
+    public class BlogController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Blog for current user.
+        [Authorize]
+        public ActionResult Index()
+        {
+            var account = HttpContext.User.Identity.Name;
+            var posts = db.Posts.Where(a => a.AccountEmail == account);
+            return View(posts);
+        }
+
+        // GET: Individual Blog for non-signed in users.
+        // public ActionResult Detail()
+        //{
+
+        //}
+    }
+}
